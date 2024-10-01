2d35ce1c8167fc3b9a9e85020bac3e40268945c8
smartrector <smartrector@gmail.com>
2024-10-01 10:44:18 +0900

    first

---
+++ Ex01_IoC/src/main/java/com/sample/spring/Ex01IoCApplication.java
@@ -0,0 +1,13 @@
+package com.sample.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex01IoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex01IoCApplication.class, args);
+	}
+
+}
