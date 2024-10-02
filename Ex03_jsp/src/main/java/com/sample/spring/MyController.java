32654f9e3b0cf97ad66e3710fd957648c5f23a92
smartrector <smartrector@gmail.com>
2024-10-02 12:27:56 +0900

    jsp

--- Ex03_jsp/src/main/java/com/sample/spring/MyController.java
+++ Ex03_jsp/src/main/java/com/sample/spring/MyController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -89,6 +90,19 @@ public class MyController {
 		model.addAttribute("name",member.getName());
 		
 		return "view2";
+	}
+	
+	@RequestMapping("/view3/{id}/{name}")
+	public String view3(
+			@PathVariable("studentId") String studentId,
+			@PathVariable("name") String name,
+			Model model
+			) {
+		
+		model.addAttribute("id", studentId);
+		model.addAttribute("name", name);
+		
+		return "view3";
 	}
 	
 	
---
+++ Ex03_jsp/src/main/webapp/WEB-INF/views/view3.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ taglib uri="jakarta.tags.core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h3>
+	이름 : ${name} <br> id : ${id}
+</h3>
+</body>
+</html>
