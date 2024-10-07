43392f0ac4f8018dbb1ccc200e73263e00499ade
smartrector <smartrector@gmail.com>
2024-10-07 14:52:37 +0900

    mybatis

---
+++ Ex06_Mybatis_simplebbs/src/main/java/com/sample/spring/Ex06MybatisSimplebbsApplication.java
@@ -0,0 +1,13 @@
+package com.sample.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex06MybatisSimplebbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex06MybatisSimplebbsApplication.class, args);
+	}
+
+}
