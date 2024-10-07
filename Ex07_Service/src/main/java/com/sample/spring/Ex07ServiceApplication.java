76376826c7a162371f89536073dd25c3b74efa6b
smartrector <smartrector@gmail.com>
2024-10-07 16:54:49 +0900

    mybatis

---
+++ Ex07_Service/src/main/java/com/sample/spring/Ex07ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sample.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex07ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex07ServiceApplication.class, args);
+	}
+
+}
