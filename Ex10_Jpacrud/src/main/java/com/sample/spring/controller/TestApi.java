90226b01d650aca0562e1b350d0f1c1d30cc9749
smartrector <smartrector@gmail.com>
2024-10-10 15:45:15 +0900

    jpacrud

--- Ex10_Jpacrud/src/main/java/com/sample/spring/controller/TestApi.java
+++ Ex10_Jpacrud/src/main/java/com/sample/spring/controller/TestApi.java
@@ -1,13 +1,19 @@
 package com.sample.spring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sample.spring.entity.TestEntity;
 import com.sample.spring.service.TestService;
 
 import lombok.AllArgsConstructor;
@@ -43,6 +49,26 @@ public class TestApi {
 			@RequestBody CreateTestRequest request
 			) {
 		testService.update(id, request.getName(), request.getAge());
+	}
+	
+//	@DeleteMapping("/test/{id}/delete")
+	@DeleteMapping("/test/delete")
+	public void deleteTest(
+//			@PathVariable("id") Long id
+			@RequestParam("id") Long id
+			) {
+		testService.delete(id);
+	}
+	
+//	@GetMapping("/test")
+//	public List<TestEntity> jsonData(){
+//		return testService.findAll(); //200 ok
+//	}
+	
+	@GetMapping("/test")
+	public ResponseEntity<List<TestEntity>> jsonData(){
+		List<TestEntity> data = testService.findAll();
+		return ResponseEntity.ok(data);
 	}
 	
 	
--- Ex10_Jpacrud/src/main/java/com/sample/spring/service/TestService.java
+++ Ex10_Jpacrud/src/main/java/com/sample/spring/service/TestService.java
@@ -1,6 +1,9 @@
 package com.sample.spring.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.sample.spring.entity.TestEntity;
@@ -21,5 +24,14 @@ public class TestService {
 		 TestEntity testEntity= testRepository.findById(id).get();
 		 testEntity.changeNameAndAge(name, age);
 		 testRepository.save(testEntity);
+	}
+	
+	public void delete(Long id) {
+		TestEntity testEntity = testRepository.findById(id).get();
+		testRepository.delete(testEntity);
+	}
+	
+	public List<TestEntity> findAll(){
+		return testRepository.findAll(Sort.by(Sort.Direction.DESC,"id"));
 	}
 }
