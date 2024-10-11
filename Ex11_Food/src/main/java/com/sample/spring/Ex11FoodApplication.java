d9c4d970d9006aef7cc441276fcac3408cf45cc9
smartrector <smartrector@gmail.com>
2024-10-11 09:49:40 +0900

    food#1

---
+++ Ex11_Food/src/main/java/com/sample/spring/Ex11FoodApplication.java
@@ -0,0 +1,13 @@
+package com.sample.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex11FoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex11FoodApplication.class, args);
+	}
+
+}
---
+++ Ex11_Food/src/test/java/com/sample/spring/Ex11FoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex11FoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
