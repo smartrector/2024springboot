c9d1c0189364759fd725eaa95c4689b920f62998
smartrector <smartrector@gmail.com>
2024-10-14 15:12:45 +0900

    todo

---
+++ Ex12_Api/src/main/java/com/sample/spring/Ex12ApiApplication.java
@@ -0,0 +1,13 @@
+package com.sample.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex12ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex12ApiApplication.class, args);
+	}
+
+}
