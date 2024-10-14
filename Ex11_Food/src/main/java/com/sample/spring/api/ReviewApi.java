a8ef29e873e9ee0de046b50b3b7e6e9d02bf9c80
smartrector <smartrector@gmail.com>
2024-10-14 14:19:55 +0900

    food

--- Ex11_Food/src/main/java/com/sample/spring/api/ReviewApi.java
+++ Ex11_Food/src/main/java/com/sample/spring/api/ReviewApi.java
@@ -1,17 +1,21 @@
 package com.sample.spring.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sample.spring.api.request.CreateReviewRequest;
 import com.sample.spring.repository.ReviewRepository;
 import com.sample.spring.sevice.ReviewService;
+import com.sample.spring.sevice.dto.ReviewDto;
 
 @RestController
 public class ReviewApi {
@@ -38,5 +42,16 @@ public class ReviewApi {
 		reviewService.deleteReview(reviewId);
 		
 	}
+	
+	@GetMapping("/food/{foodId}/reviews")
+	public ReviewDto getfoodReviews(
+			@PathVariable("foodId") Long foodId,
+			@RequestParam("offset") Integer offset,
+			@RequestParam("limit") Integer limit
+			) {
+		return reviewService.getFoodReview(foodId, PageRequest.of(offset/limit, limit));
+	}
+	
+	
 
 }
