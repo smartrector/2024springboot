0b4010bea0a54064c9370f64bf2d1fefadb4607a
smartrector <smartrector@gmail.com>
2024-10-15 16:54:28 +0900

    fileup

---
+++ Ex13_FileUpload/src/main/java/com/sample/spring/Ex13FileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.sample.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex13FileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex13FileUploadApplication.class, args);
+	}
+
+}
