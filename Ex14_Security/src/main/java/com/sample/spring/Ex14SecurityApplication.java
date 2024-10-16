96a27a222207809c43b613073247a92bb1025585
smartrector <smartrector@gmail.com>
2024-10-16 14:52:27 +0900

    security

---
+++ Ex14_Security/src/main/java/com/sample/spring/Ex14SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.sample.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex14SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex14SecurityApplication.class, args);
+	}
+
+}
